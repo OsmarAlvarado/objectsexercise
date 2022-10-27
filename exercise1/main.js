2d6d3b36b0aaafa1a5a03b9bc14e8ae891d8e870
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-10-27 18:17:34 +0200

    folders

---
+++ exercise1/main.js
@@ -0,0 +1,27 @@
+let product = {
+    name: 'headphone',
+    price: 120,
+    doesItWork: false,
+    previousOwner: 'bob'
+}
+
+for (const keys in product) {
+    console.log(`${keys}: ${product[keys]}`);
+}
+
+delete product.previousOwner;
+console.log(product);
+
+product.doesItWork = true
+console.log(product);
+
+if (product.price > 100) {
+    product.discountPercentage = product.price * .10
+}
+console.log(product);
+
+let discountPercentage = product.price * .10
+product.price = (product.price - discountPercentage)
+
+console.log(product);
+
